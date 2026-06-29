4ab6bf9e448f29c60382dc7535087db69a2b3961
Vitor Oliveira <vitorxd12.12@gmail.com>
2026-06-29 10:33:28 -0300

    feat: configure Spring Boot application and properties

---
+++ src/main/java/com/example/nasa_apod/NasaApodApplication.java
@@ -0,0 +1,13 @@
+package com.example.nasa_apod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NasaApodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NasaApodApplication.class, args);
+	}
+
+}
